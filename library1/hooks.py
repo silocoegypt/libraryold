0b5e8f72b73821df0dba24d63d0940581266c5de
silocoegypt <silocoegypt@gmail.com>
2021-11-20 19:45:18 +0100

    TEST1

--- library1/hooks.py
+++ library1/hooks.py
@@ -14,7 +14,7 @@ app_license = "MIT"
 
 # include js, css files in header of desk.html
 # app_include_css = "/assets/library1/css/library1.css"
-# app_include_js = "/assets/library1/js/library1.js"
+# app_include_js = ["https://maps.googleapis.com/maps/api/js?libraries=places"]
 
 # include js, css files in header of web template
 # web_include_css = "/assets/library1/css/library1.css"
