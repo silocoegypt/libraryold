f9d8f8d8475f3454b8f4f8075ae6aa96c440a1e9
Wael Ahmed <57874622+silocoegypt@users.noreply.github.com>
2021-12-03 01:47:50 +0200

    Delete library1/library1/doctype directory

--- library1/library1/doctype/mapgeo/mapgeo.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2021, Wael and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class mapgeo(Document):
-	pass
--- library1/library1/doctype/mapsearch/mapsearch.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2021, Wael and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('mapsearch', {
-	// refresh: function(frm) {
-
-	// }
-});
